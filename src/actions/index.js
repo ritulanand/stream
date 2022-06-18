f7f04a68d350a7f96c1c10f7ebd0d9640ac48b20
ritulanand <ritulanand1997@gmail.com>
2022-06-18 17:45:54 +0530

    stram react project

---
+++ src/actions/index.js
@@ -0,0 +1,48 @@
+import { formValues } from 'redux-form';
+import streams from '../apis/streams';
+import history from '../history';
+
+import { SIGN_OUT, SIGN_IN, CREATE_STREAM , FETCH_STREAMS, 
+    FETCH_STREAM, DELETE_STREAM, EDIT_STREAM
+ 
+} from "./types";
+
+export const signIn = () => {
+    return {
+        type: SIGN_IN
+    };
+};
+
+export const signOut = () => {
+    return {
+        type: SIGN_OUT
+    };
+};
+
+export const createStream = formValues => async (dispatch, getState) => {
+    const { userId } = getState().auth;
+   
+   const response = await streams.post('/streams', {...formValues, userId});
+   dispatch({ type: CREATE_STREAM, payload: response.data})
+   console.log(userId);
+   history.push('/');
+};
+
+export const fetchStreams = () => async dispatch => {
+    const response = await streams.get('/streams');
+    dispatch({type: FETCH_STREAMS, payload: response.data});
+};
+export const fetchStream = (id) => async dispatch => {
+    const response = await streams.get(`/streams/${id}`);
+    dispatch({type: FETCH_STREAM, payload: response.data});
+};
+
+export const editStream = (id, formValues) => async dispatch => {
+    const response = await streams.put(`/streams/${id}`, formValues);
+    dispatch({type: EDIT_STREAM, payload: response.data});
+};
+export const deleteStream =(id) => async dispatch => {
+    await streams.delete(`/stream/${id}`);
+    dispatch({type: DELETE_STREAM, payload: id});
+};
+
